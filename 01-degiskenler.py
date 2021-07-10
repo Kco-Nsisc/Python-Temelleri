65d37a08eefe0737e70f03f5630ccdc03a0bfaff
Kco-Nsisc <86565755+Kco-Nsisc@users.noreply.github.com>
2021-07-10 16:06:13 +0300

    Add files via upload

---
+++ 01-degiskenler.py
@@ -0,0 +1,34 @@
+# Değişkenler
+# Değişkenler bir değere eşitlendiken sonra RAM e kaydedilir. Hadi eşitleme yapalım.
+
+
+x = 4
+# Tahmin edebileceğiniz gibi x değişkenini 4 sayısına eşitledik.
+
+
+a = 5
+
+print(a * x) #print() bir fonksiyon fonksiyon ne dersen şimdilik onu bilmene gerek yok sadece şunu bilsen yeter print içindeki değişkense değişkeni, yazıysa yazıyı, sayıysa sayıyı, işlemse sonucu vb. yazdırır
+
+# Buradaki * işareti çarpma işlemidir. Daha sonra daha detaylı olarak göreceğiz.
+
+
+print(x)
+
+
+
+x = 10
+
+x * a
+
+
+# Burada olan şu kodlar sırayla çalışıyor yani yukarıdan aşağı ilk başta x' in değeri 4' tü sonra x 10 oldu ona göre işlem yaptı 
+
+Kod = 7
+
+print(Kod) 
+
+
+Kod = 5 #burada Kod değişkenini 5 değerine atadım yani ilk başta Kod'un 7 olduğunu söyledim sonra dedim ki, Kod artık 7
+
+print(Kod)
